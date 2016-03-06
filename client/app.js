1a9cb3330a0a7241976bd613e6dc6277e60d6e4e
seemajune <seemashariat@gmail.com>
2016-03-06 11:42:40 -0500

    replaces messages list with template helper and each block, reorganizes file structure with components subdir to house templates

---
+++ client/app.js
@@ -0,0 +1,11 @@
+Template.messages.helpers({
+  messages: [
+    { text: "All of these messages" },
+    { text: "Uses the same template" },
+    { text: "But have different data contexts" },
+    { text: "That's why we can see all these different messages" },
+    { text: "Hey!" },
+    { text: "What up man?" },
+    { text: "Ello friend!" }
+  ]
+});
